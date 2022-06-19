19838fbf0170397b546214a814e6a22b7643f576
Igor Polishchuk <igor.policee@gmail.com>
2022-06-19 19:36:22 +0300

    version 2

--- src/data_load_shipping.sql
+++
@@ -1,8 +0,0 @@
--- Загрузка данных из файла источника shipping.csv в таблицу "shipping" (с помощью скрипта или используя клиент)
-COPY
-    online_store.public.shipping
-FROM
-    'data/shipping.csv'
-DELIMITER
-    ','
-CSV HEADER;
